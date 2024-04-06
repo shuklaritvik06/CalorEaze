6dbeb8b7cc383675b90166830c75643ae41ec6cf
Ritvik Shukla <ritvikshukla261@gmail.com>
2024-04-06 21:24:33 +0530

    structure creation

--- api/settings/base.py
+++ api/settings/base.py
@@ -146,9 +146,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "assets")]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
